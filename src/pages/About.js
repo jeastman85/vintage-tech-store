4207fe9a7ddad1455c4721cf858d1192109af2bf
jeastman85 <jeff.eastman@outlook.com>
2020-09-13 07:18:26 -0400

    Files Added

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Deep v synth cloud bread readymade ugh vaporware you probably haven't
+        heard of them commodo brunch pinterest. Trust fund nulla hot chicken sed
+        lo-fi etsy cronut hoodie knausgaard listicle cliche chartreuse labore
+        health goth. Skateboard ennui aliqua chillwave vinyl, tbh non laborum
+        mumblecore gluten-free. Pork belly af church-key woke street art la
+        croix pok pok master cleanse scenester biodiesel gochujang.
+      </p>
+    </section>
+  );
+}
---
+++ src/utils/URL.js
@@ -0,0 +1,9 @@
+// url
+//export default "http://localhost:1337";
+
+export default "https://je-tech-store-api.herokuapp.com";
+//install axios
+
+//get url
+
+//response
